8f317e416011d0ad9c48be1c9022f6ac796aa7e6
1111 <a1111@1111s-MacBook-Pro.local>
2024-11-13 05:05:09 -0800

    advocate challenge done

---
+++ graphql/schemas/index.ts
@@ -0,0 +1,37 @@
+import { TaskModel } from "@/mongoose/type";
+import { gql } from "apollo-server-cloud-functions";
+
+export const typeDefs = gql`
+  scalar Date
+
+  type Task {
+    _id: ID!
+    taskName: String!
+    priority: Int!
+    isDone: Boolean
+    createdAt: Date!
+    updatedAt: Date!
+  }
+
+  input TaskInput {
+    taskName: String!
+    priority: Int!
+  }
+
+  input UpdateTaskInput {
+    _id: ID!
+    taskName: String!
+    priority: Int!
+    isDone: Boolean!
+  }
+
+  type Query {
+    getAllTasks: [Task!]!
+    getDoneTaskLists: [Task!]!
+  }
+
+  type Mutation {
+    createTask(input: TaskInput): Task!
+    updateTask(input: UpdateTaskInput): Task!
+  }
+`;
